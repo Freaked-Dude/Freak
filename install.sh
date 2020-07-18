cd9db425ab296fdf10019e7270fcf20ed9049a74
Freaked-Dude <66533529+Freaked-Dude@users.noreply.github.com>
2020-07-18 05:28:49 +0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#apt update && apt upgrade && apt install python python2 git && pip2 install mechanize requests 
+#git clone https://github.com/Freaked-Dude/Freak 
+#cd Freak python2 Freak
+#Username: Freak
+#Password: Login
