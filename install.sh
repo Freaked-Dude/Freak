a474340633a4b985e577fd6d0b747d12e5dab36e
Freaked-Dude <66533529+Freaked-Dude@users.noreply.github.com>
2020-07-18 05:33:04 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-#apt update && apt upgrade && apt install python python2 git && pip2 install mechanize requests 
-#git clone https://github.com/Freaked-Dude/Freak 
-#cd Freak python2 Freak
-#Username: Freak
-#Password: Login
+apt update && apt upgrade && apt install python python2 git && pip2 install mechanize requests 
+git clone https://github.com/Freaked-Dude/Freak 
+cd Freak python2 Freak
+
